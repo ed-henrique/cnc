efbd715c4842abc463ff9f34a9ecf03d63f0bfe6
veniciusjacob <veniciusjacob@hotmail.com>
2022-07-23 23:31:00 -0400

    client/server chatroom multithreading update
    Signed-off-by: veniciusjacob <veniciusjacob@hotmail.com>

--- client_thread.c
+++ client_thread.c
@@ -10,25 +10,26 @@
 #include <pthread.h>
 
 #define LENGTH 2048
+#define LEN 32
 
 // Global variables
 volatile sig_atomic_t flag = 0;
 int sockfd = 0;
-char name[32];
+char name[LEN];
 
 void str_overwrite_stdout() {
-  printf("%s", "> ");
-  fflush(stdout);
+    printf("%s", "> ");
+    fflush(stdout);
 }
 
 void str_trim_lf (char* arr, int length) {
-  int i;
-  for (i = 0; i < length; i++) { // trim \n
-    if (arr[i] == '\n') {
-      arr[i] = '\0';
-      break;
+    int i;
+    for (i = 0; i < length; i++) { // trim \n
+        if (arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
     }
-  }
 }
 
 void catch_ctrl_c_and_exit(int sig) {
@@ -36,41 +37,41 @@ void catch_ctrl_c_and_exit(int sig) {
 }
 
 void send_msg_handler() {
-  char message[LENGTH] = {};
-	char buffer[LENGTH + 32] = {};
+    char message[LENGTH] = {};
+	char buffer[LENGTH + LEN] = {};
 
-  while(1) {
-  	str_overwrite_stdout();
-    fgets(message, LENGTH, stdin);
-    str_trim_lf(message, LENGTH);
+    while(1) {
+  	    str_overwrite_stdout();
+        fgets(message, LENGTH, stdin);
+        str_trim_lf(message, LENGTH);
 
-    if (strcmp(message, "exit") == 0) {
+        if (strcmp(message, "exit") == 0) {
 			break;
-    } else {
-      sprintf(buffer, "%s: %s\n", name, message);
-      send(sockfd, buffer, strlen(buffer), 0);
-    }
+        } else {
+            sprintf(buffer, "%s: %s\n", name, message);
+            send(sockfd, buffer, strlen(buffer), 0);
+        }
 
 		bzero(message, LENGTH);
-    bzero(buffer, LENGTH + 32);
-  }
-  catch_ctrl_c_and_exit(2);
+        bzero(buffer, LENGTH + LEN);
+    }
+    catch_ctrl_c_and_exit(2);
 }
 
 void recv_msg_handler() {
-	char message[LENGTH] = {};
-  while (1) {
+    char message[LENGTH] = {};
+    while (1) {
 		int receive = recv(sockfd, message, LENGTH, 0);
-    if (receive > 0) {
-      printf("%s", message);
-      str_overwrite_stdout();
-    } else if (receive == 0) {
-			break;
-    } else {
-			// -1
-		}
+        if (receive > 0) {
+        printf("%s", message);
+        str_overwrite_stdout();
+        } else if (receive == 0) {
+		break;
+        } else {
+		    -1;
+	    }
 		memset(message, 0, sizeof(message));
-  }
+    }
 }
 
 int main(int argc, char **argv){
@@ -85,11 +86,11 @@ int main(int argc, char **argv){
 	signal(SIGINT, catch_ctrl_c_and_exit);
 
 	printf("Please enter your name: ");
-  fgets(name, 32, stdin);
-  str_trim_lf(name, strlen(name));
+    fgets(name, LEN, stdin);
+    str_trim_lf(name, strlen(name));
 
 
-	if (strlen(name) > 32 || strlen(name) < 2){
+	if (strlen(name) > LEN || strlen(name) < 2){
 		printf("Name must be less than 30 and more than 2 characters.\n");
 		return EXIT_FAILURE;
 	}
@@ -97,32 +98,32 @@ int main(int argc, char **argv){
 	struct sockaddr_in server_addr;
 
 	/* Socket settings */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-  server_addr.sin_port = htons(port);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
 
 
   // Connect to Server
-  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (err == -1) {
+    int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (err == -1) {
 		printf("ERROR: connect\n");
 		return EXIT_FAILURE;
 	}
 
 	// Send name
-	send(sockfd, name, 32, 0);
+	send(sockfd, name, LEN, 0);
 
 	printf("=== WELCOME TO THE CHATROOM ===\n");
 
 	pthread_t send_msg_thread;
-  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
+    if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
 		printf("ERROR: pthread\n");
-    return EXIT_FAILURE;
+        return EXIT_FAILURE;
 	}
 
 	pthread_t recv_msg_thread;
-  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
+    if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
 		printf("ERROR: pthread\n");
 		return EXIT_FAILURE;
 	}
@@ -131,7 +132,7 @@ int main(int argc, char **argv){
 		if(flag){
 			printf("\nBye\n");
 			break;
-    }
+        }
 	}
 
 	close(sockfd);
